4b76cb1c4d96794c813bd2b39f17a82c74833d81
Michelle <hello@michellecr.uz>
2020-05-26 15:54:50 -0400

    includes build

---
+++ build/precache-manifest.a700739fd99f389b6360579d1e842c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1e9da0c0dad1962bd364b3d575812a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55844868e5dd3c0d0026",
+    "url": "/static/css/main.0144805f.chunk.css"
+  },
+  {
+    "revision": "fbc493cbae18ac1c97b0",
+    "url": "/static/js/2.d307d41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d307d41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55844868e5dd3c0d0026",
+    "url": "/static/js/main.0310e7d5.chunk.js"
+  },
+  {
+    "revision": "60d3f17995c0214425cf",
+    "url": "/static/js/runtime-main.1046ec41.js"
+  }
+]);
