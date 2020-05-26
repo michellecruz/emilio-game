e4fe2576de561b86c89bb78c9580710ff8c98330
Michelle <hello@michellecr.uz>
2020-05-26 12:59:19 -0400

    much better

--- src/Test.js
+++ src/Test.js
@@ -1,44 +1,37 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
 
-let camera, scene, renderer, raycaster;
+let camera, scene, renderer;
 let geometry, texture, textureOpen, material, mesh;
 let geometryKibble, textureKibble, materialKibble, meshKibble;
-let INTERSECTED;
 
 class Test extends Component {
-  state = {
-    emilio: {
-      x: -9,
-      y: 9,
-    }
-  }
-  
+
   init = () => {
-    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.set( 0, 0, 10 );
+    camera = new THREE.PerspectiveCamera( 1, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.set( 0, 3, 1000 );
   
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xffffff );
-    scene.add( camera );
 
-    geometry = new THREE.SphereBufferGeometry( 1, 32, 32 );
+    geometry = new THREE.SphereBufferGeometry( 4.5, 32, 32 );
     texture = new THREE.TextureLoader().load( 'img/milio3.jpg' );
     textureOpen = new THREE.TextureLoader().load( 'img/milio3-open.jpg' );
     material = new THREE.MeshBasicMaterial( { map: texture } );
 
     mesh = new THREE.Mesh( geometry, material );
+
+    mesh.position.x = -16
+    mesh.position.y = 16
+    mesh.position.z = 1
+
     mesh.rotation.x = 0.2;
     mesh.rotation.y = 6.6;
     mesh.rotation.z = 5;
-    mesh.position.y = this.state.emilio.y;
-    mesh.position.x = this.state.emilio.x;
-
     mesh.material.map.needsUpdate = true;
   
-    scene.add( mesh );
     this.kibble();
-
+    scene.add( mesh );
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -49,22 +42,30 @@ class Test extends Component {
   kibble = () => {
     geometryKibble = new THREE.SphereBufferGeometry( 1, 32, 32 );
     textureKibble = new THREE.TextureLoader().load( 'img/kibble.png' );
-    materialKibble = new THREE.MeshBasicMaterial( { map: textureKibble, color: 0xff0000, transparent: true } );
+    materialKibble = new THREE.MeshBasicMaterial( { map: textureKibble, transparent: true } );
     meshKibble = new THREE.Mesh( geometryKibble, materialKibble );
     meshKibble.rotation.y = 10
 
     meshKibble.position.x = 10
-    meshKibble.position.y = 0
-    meshKibble.position.z = 10
+    meshKibble.position.y = 10
+    meshKibble.position.z = 8
     
     scene.add( meshKibble );
+
+
+
+    
+
+
+
   }
 
   animate = () => {
     requestAnimationFrame( this.animate );
 
     if (mesh.position.y > 0) {
-      mesh.position.y -= 0.1;
+      mesh.position.y -= 0.2;
+      mesh.position.x += 0.1;
     } else {
       mesh.material.map = texture;
     }
@@ -72,37 +73,29 @@ class Test extends Component {
     mesh.position.x += 0.05;
 
     if (mesh.position.y > 0) {
-      mesh.rotation.z -= 0.05 * -mesh.position.y;
+      if (mesh.position.x < 0) {
+        mesh.rotation.z -= 0.05 * -mesh.position.y;
+      } else {
+        mesh.rotation.z -= 0.01 * -mesh.position.y;
+      }
     } else {
       mesh.rotation.z -= 0.05
     }
     
     if (mesh.position.x > 0) {
-      camera.position.x += 0.05;
+      camera.position.x = mesh.position.x;
     }
 
     renderer.render( scene, camera );
-    
 
-
-    if (mesh.position.x > 0) {
-      raycaster = new THREE.Raycaster();
-      raycaster.setFromCamera(meshKibble.position, camera);
-      let intersects = raycaster.intersectObjects( scene.children );
-      console.log(intersects.length, meshKibble.position, camera.position);
-    }
-
-      // if ( intersects.length > 0 ) {
-      //   if ( INTERSECTED !== intersects[ 0 ].object ) {
-      //     if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-      //     INTERSECTED = intersects[ 0 ].object;
-      //     INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-      //     INTERSECTED.material.emissive.setHex( 0xff0000 );
-      //   }
-      // } else {
-      //   if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-      //   INTERSECTED = null;
-      // }
+    console.log(
+      this.spheresIntersect(
+        meshKibble.geometry.boundingSphere,
+        meshKibble.position,
+        mesh.geometry.boundingSphere,
+        mesh.position,
+      )
+    );
   }
 
   changePosition = () => {
@@ -112,16 +105,22 @@ class Test extends Component {
     if (mesh.position.x > 0) {
       camera.position.x += 1;
     }
-    
-    if (mesh.position.x < 0) {
-      mesh.rotation.z += 0.01 * -mesh.position.x;
-    }
 
-    if (mesh.position.y > -3) {
-      mesh.position.y += 4 * (mesh.position.y * 0.1);
+    if (mesh.position.y > 0) {
+      mesh.position.y += 2 * -(mesh.position.y * 0.01);
     }
 
     mesh.material.map = textureOpen;
+  }
+
+  spheresIntersect = (sphere1, sphere1position, sphere2, sphere2position) => {
+    function distanceVector( v1, v2 ) {
+        var dx = v1.x - v2.x;
+        var dy = v1.y - v2.y;
+
+        return Math.sqrt( dx * dx + dy * dy );
+    }
+    return distanceVector(sphere1position, sphere2position) <= (sphere1.radius + sphere2.radius)
   }
 
   componentDidMount() {
@@ -130,12 +129,6 @@ class Test extends Component {
 
     document.addEventListener("keydown", e =>
       e.keyCode === 32 ?
-      // this.setState({
-      //   emilio: {
-      //     x: 0,
-      //     y: 0.1,
-      //   }
-      // },
       requestAnimationFrame(this.changePosition) : null
     );
 
